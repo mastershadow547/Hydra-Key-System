d36f11689d6f3d876da4e7291eba78e95cae6109
mastershadow547 <mastershadow547@gmail.com>
2026-04-21 20:09:15 -0400

    Update generate_key.py

--- generate_key.py
+++ generate_key.py
@@ -1,1 +1,9 @@
+import random
+import string
 
+new_key = "Hydra_" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
+
+with open("key.txt", "w") as f:
+    f.write(new_key)
+
+print(f"New key generated: {new_key}")
